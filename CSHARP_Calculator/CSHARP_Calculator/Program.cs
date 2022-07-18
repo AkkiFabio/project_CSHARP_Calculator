57258992c0152a18bc4f357688db23cf88ad5962
Fabio <akki.fabio@gmail.com>
2022-07-18 10:30:38 -0300

    Enviando calculadora

---
+++ CSHARP_Calculator/CSHARP_Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSHARP_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+            int contador;
+
+
+            Console.WriteLine("Digite um número para ver sua tabuada");
+
+            valor = double.Parse(Console.ReadLine());
+            Console.WriteLine("Você escolheu o número " + valor + ", abaixo segue a tabuada o multiplicando por 1 a 10:");
+
+            for (contador = 1; contador <= 10; contador++)
+            {
+                Console.WriteLine(valor + " x " + contador + " = " + valor * contador + "\n");
+            }
+
+            Console.WriteLine("Fim do programa");
+            Console.Read();
+        }
+    }
+}
